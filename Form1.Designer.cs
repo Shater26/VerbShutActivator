fe54828238a3241e43b2b41b5a37d30cf78662a6
Fernando Afonso <faonso56@outlook.com>
2025-02-19 22:51:30 +0100

    0.1.0 Alpha

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace VerbShutActivator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SuspendLayout();
+
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(225, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+
+            this.btnEnable.Location = new System.Drawing.Point(38, 24);
+            this.btnEnable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(150, 35);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Activar mensajes de apagado avanzados";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+
+            this.btnDisable.Location = new System.Drawing.Point(38, 73);
+            this.btnDisable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(150, 34);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Text = "Desactivar mensajes de apagado avanzados";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 130);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Activador de mensajes de apagado avanzado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace VerbShutActivator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+
+            if (EnableVerboseShutdownPolicy())
+            {
+                MessageBox.Show("Mensajes detallados activados correctamente mediante Directivas de Grupo Local.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else if (EnableVerboseShutdownRegistry())
+            {
+                MessageBox.Show("Este usuario no puede editar Directivas de Grupo Local. Mensajes detallados activados correctamente mediante el Registro de Windows.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudieron editar las Directivas de Grupo Local. Intente ejecutar el programa de nuevo como administrador. Los cambios no han sido guardados en el Equipo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+
+            if (DisableVerboseShutdownPolicy())
+            {
+                MessageBox.Show("Mensajes detallados desactivados correctamente mediante Directivas de Grupo Local.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else if (DisableVerboseShutdownRegistry())
+            {
+                MessageBox.Show("Este usuario no puede editar Directivas de Grupo Local. Mensajes detallados desactivados correctamente mediante el Registro de Windows.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudieron editar las Directivas de Grupo Local. Intente ejecutar el programa de nuevo como administrador. Los cambios no han sido guardados en el Equipo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool EnableVerboseShutdownPolicy()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System", true))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue("VerboseStatus", 1, RegistryValueKind.DWord);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private bool EnableVerboseShutdownRegistry()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", true))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue("verbosestatus", 1, RegistryValueKind.DWord);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private bool DisableVerboseShutdownPolicy()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System", true))
+                {
+                    if (key != null)
+                    {
+                        key.SetValue("VerboseStatus", 0, RegistryValueKind.DWord);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private bool DisableVerboseShutdownRegistry()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System", true))
+                {
+                    if (key != null)
+                    {
+                        key.DeleteValue("verbosestatus", false);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
