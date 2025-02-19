002ebe3640c9f87b495abb2938ccfecd544bcb1e
Fernando Afonso <faonso56@outlook.com>
2025-02-19 22:59:48 +0100

    0.1.1 Alpha

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -18,8 +18,33 @@
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(38, 24);
+            this.btnEnable.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(150, 35);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Activar mensajes de apagado avanzados";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Location = new System.Drawing.Point(38, 73);
+            this.btnDisable.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(150, 34);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Text = "Desactivar mensajes de apagado avanzados";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // menuStrip1
+            // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.archivoToolStripMenuItem,
             this.editarToolStripMenuItem,
@@ -29,37 +54,27 @@
             this.menuStrip1.Size = new System.Drawing.Size(225, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
-
+            // 
+            // archivoToolStripMenuItem
+            // 
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
             this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
-
+            // 
+            // editarToolStripMenuItem
+            // 
             this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
             this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
             this.editarToolStripMenuItem.Text = "Editar";
-
+            // 
+            // ayudaToolStripMenuItem
+            // 
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
             this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
-
-            this.btnEnable.Location = new System.Drawing.Point(38, 24);
-            this.btnEnable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnEnable.Name = "btnEnable";
-            this.btnEnable.Size = new System.Drawing.Size(150, 35);
-            this.btnEnable.TabIndex = 0;
-            this.btnEnable.Text = "Activar mensajes de apagado avanzados";
-            this.btnEnable.UseVisualStyleBackColor = true;
-            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
-
-            this.btnDisable.Location = new System.Drawing.Point(38, 73);
-            this.btnDisable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnDisable.Name = "btnDisable";
-            this.btnDisable.Size = new System.Drawing.Size(150, 34);
-            this.btnDisable.TabIndex = 1;
-            this.btnDisable.Text = "Desactivar mensajes de apagado avanzados";
-            this.btnDisable.UseVisualStyleBackColor = true;
-            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
-
+            // 
+            // Form1
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(225, 130);
@@ -67,10 +82,12 @@
             this.Controls.Add(this.btnDisable);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Activador de mensajes de apagado avanzado";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
